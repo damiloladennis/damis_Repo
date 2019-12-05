37e321aea7986d1dbe5e36b78d6bdb74976ad76e
DezezBoi <damiloladennis@gmail.com>
2019-12-05 15:40:28 +0000

    Dynamodb_training_folcer

---
+++ Dynamodb_training/test-setup/jsonfile.js
@@ -0,0 +1,13 @@
+{
+	"name": "Riyaz",
+	"age": 35,
+	"loves programming": true,
+	"contact": {
+		"email": "skunk@life.com",
+		"website": "www.life.com"
+	},
+	"language known": [
+		"Node.js", "PHP", "Python", "Ruby"
+	],
+	"can be null": null
+}
